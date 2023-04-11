767b47921ac3979ead0dfdad8b309c44188cfe6e
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-11 22:57:44 +0600

    first commit

---
+++ src/pages/about/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <Head> <title> About || local cleaning service </title> </Head>
+            <h2 className="text-3xl">About Page</h2>
+        </div>
+    );
+};
+
+export default index;
---
+++ src/pages/contact/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const index = () => {
+  return (
+    <div>
+      <Head>
+        <title> Contact || local cleaning service </title>
+      </Head>
+      <h2 className="text-3xl">Contact Page</h2>
+    </div>
+  );
+};
+
+export default index;
