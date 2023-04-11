c2d2b205ca10b09e6ad3e446595209e46a010dac
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-04-11 23:13:20 +0600

    cleaning hero section

---
+++ components/HeroSection.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url("/carpet_cleaning_banner.jpg")` }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Carpet Cleaning Service </h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import Navbar from '../../components/Navbar'
+import HeroSection from '../../components/HeroSection'
 
 export default function App({ Component, pageProps }) {
   return (
   <div>
     <Navbar/>
+    <HeroSection/>
     <Component {...pageProps} />
   </div>)
 }
